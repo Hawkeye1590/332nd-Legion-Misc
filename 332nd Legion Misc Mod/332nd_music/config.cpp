60337961298938228d4a0069cde0478f73965ca0
Hawkeye <125225357+Hawkeye1590@users.noreply.github.com>
2024-01-08 20:42:49 -0500

    Add files via upload

---
+++ 332nd Legion Misc Mod/332nd_music/config.cpp
@@ -0,0 +1,427 @@
+class CfgPatches
+{
+	class ORG_Music
+	{
+		name="ORG Music";
+		author="Magic";
+		units[]={};
+		weapons[]={};
+	};
+};
+class CfgMusic
+{
+	tracks[]=
+	{
+		"Stop_Music",
+		"Clone_Army_March",
+		"Droid_Army_March",
+		"Burying_the_Dead",
+		"Ahsoka_vs_Maul",
+		"Siege_of_Mandalore",
+		"Im_So_Sorry",
+		"Shattered",
+		"Vode_An",
+		"Vode_An_and_Clones",
+		"Mandalorian_War_Song",
+		"Mandalorian_War_Chant",
+		"Bad_Batch",
+		"Crosshair_and_Bad_Batch",
+		"Force_Theme",
+		"We_Dont_Understand",
+		"Maelstorm",
+		"Valentinos",
+		"Militech",
+		"Cyberpsycho",
+		"NCPD",
+		"Dooku_Arrives",
+		"Soldiers_are_Here",
+		"Spank",
+		"Ass_Clap",
+		"How_Strong_are_You",
+		"Aniki_of_the_Night",
+		"No_More_Suffering",
+		"The_Inquisitor",
+		"Ahsokas_Village",
+		"Dookus_Fall",
+		"Past_Present_Suite",
+		"The_Night_Before"
+	};
+	class Stop_Music
+	{
+		name="Stop Music";
+		musicClass="ORG_Music_Other";
+		sound[]=
+		{
+			"\ORG_music\data\Other\Stop.ogg",
+			1,
+			1
+		};
+		duration="1";
+	};
+	class Clone_Army_March
+	{
+		name="Clone Army March (Action)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Clone_Army_March.ogg",
+			1,
+			1
+		};
+		duration="324";
+	};
+	class Droid_Army_March
+	{
+		name="Droid Army March (Action)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Droid_Army_March.ogg",
+			1,
+			1
+		};
+		duration="206";
+	};
+	class Burying_the_Dead
+	{
+		name="Burying the Dead (Calm, Sad)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Burying_the_Dead.ogg",
+			1,
+			1
+		};
+		duration="136";
+	};
+	class Ahsoka_vs_Maul
+	{
+		name="Ahsoka vs Maul (Action)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Ahsoka_vs_Maul.ogg",
+			1,
+			1
+		};
+		duration="183";
+	};
+	class Siege_of_Mandalore
+	{
+		name="Siege of Mandalore (Action)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Siege_of_Mandalore.ogg",
+			1,
+			1
+		};
+		duration="218";
+	};
+	class Im_So_Sorry
+	{
+		name="I'm So Sorry (Calm, Sad)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Im_So_Sorry.ogg",
+			1,
+			1
+		};
+		duration="307";
+	};
+	class Shattered
+	{
+		name="Shattered (Calm)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Shattered.ogg",
+			1,
+			1
+		};
+		duration="301";
+	};
+	class Vode_An
+	{
+		name="Vode An (Action)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Vode_An.ogg",
+			1,
+			1
+		};
+		duration="240";
+	};
+	class Vode_An_and_Clones
+	{
+		name="Vode An & Clones (Action)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Vode_An_and_Clones_Theme.ogg",
+			1,
+			1
+		};
+		duration="193";
+	};
+	class Mandalorian_War_Song
+	{
+		name="Mandalorian War Song (Action)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Mandalorian_War_Song.ogg",
+			1,
+			1
+		};
+		duration="193";
+	};
+	class Mandalorian_War_Chant
+	{
+		name="Mandalorian War Chant (Action)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Mandalorian_War_Chant.ogg",
+			1,
+			1
+		};
+		duration="203";
+	};
+	class Bad_Batch
+	{
+		name="The Bad Batch (Action)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Bad_Batch.ogg",
+			1,
+			1
+		};
+		duration="274";
+	};
+	class Crosshair_and_Bad_Batch
+	{
+		name="Crosshair & The Bad Batch (Action)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Crosshair_and_Bad_Batch.ogg",
+			1,
+			1
+		};
+		duration="321";
+	};
+	class Force_Theme
+	{
+		name="The Force Theme (Calm)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Force_Theme.ogg",
+			1,
+			1
+		};
+		duration="224";
+	};
+	class We_Dont_Understand
+	{
+		name="We Don't Understand (Action, Foreboding)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\We_Dont_Understand.ogg",
+			1,
+			1
+		};
+		duration="146";
+	};
+	class Dooku_Arrives
+	{
+		name="Dooku Arrives (Calm, Defeat)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Dooku_Arrives.ogg",
+			1,
+			1
+		};
+		duration="164";
+	};
+	class Soldiers_are_Here
+	{
+		name="Soldiers are Here (Action, Foreboding)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Soldiers_are_Here.ogg",
+			1,
+			1
+		};
+		duration="291";
+	};
+	class No_More_Suffering
+	{
+		name="No More Suffering (Calm, Victory)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\No_More_Suffering.ogg",
+			1,
+			1
+		};
+		duration="143";
+	};
+	class The_Inquisitor
+	{
+		name="The Inquisitor (Foreboding)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\The_Inquisitor.ogg",
+			1,
+			1
+		};
+		duration="224";
+	};
+	class Ahsokas_Village
+	{
+		name="Ahsoka's Village (Calm)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Ahsokas_Village.ogg",
+			1,
+			1
+		};
+		duration="167";
+	};
+	class Dookus_Fall
+	{
+		name="Dooku's Fall (Calm)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Dookus_Fall.ogg",
+			1,
+			1
+		};
+		duration="164";
+	};
+	class Past_Present_Suite
+	{
+		name="Past/Present Suite (Hope)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Past_Present_Suite.ogg",
+			1,
+			1
+		};
+		duration="224";
+	};
+	class The_Night_Before
+	{
+		name="The Night Before (Calm)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\The_Night_Before.ogg",
+			1,
+			1
+		};
+		duration="118";
+	};
+	class Eternal_Empire_Theme
+	{
+		name="Eternal Empire Theme (Action)";
+		musicClass="ORG_Music_SW";
+		sound[]=
+		{
+			"\ORG_music\data\SW\Eternal_Empire_Theme.ogg",
+			1,
+			1
+		};
+		duration="154";
+	};
+	class Maelstorm
+	{
+		name="Maelstorm Combat Theme (Action)";
+		musicClass="ORG_Music_CP2077";
+		sound[]=
+		{
+			"\ORG_music\data\CP2077\Maelstorm.ogg",
+			1,
+			1
+		};
+		duration="219";
+	};
+	class Valentinos
+	{
+		name="Valentinos Combat Theme (Action)";
+		musicClass="ORG_Music_CP2077";
+		sound[]=
+		{
+			"\ORG_music\data\CP2077\Valentinos.ogg",
+			1,
+			1
+		};
+		duration="305";
+	};
+	class Militech
+	{
+		name="Militech Combat Theme (Action)";
+		musicClass="ORG_Music_CP2077";
+		sound[]=
+		{
+			"\ORG_music\data\CP2077\Militech.ogg",
+			1,
+			1
+		};
+		duration="408";
+	};
+	class Cyberpsycho
+	{
+		name="Cyberpsycho Combat Theme (Action)";
+		musicClass="ORG_Music_CP2077";
+		sound[]=
+		{
+			"\ORG_music\data\CP2077\Cyberpsycho.ogg",
+			1,
+			1
+		};
+		duration="233";
+	};
+	class NCPD
+	{
+		name="NCPD Combat Theme (Action)";
+		musicClass="ORG_Music_CP2077";
+		sound[]=
+		{
+			"\ORG_music\data\CP2077\NCPD.ogg",
+			1,
+			1
+		};
+		duration="296";
+	};
+};
+class CfgMusicClasses
+{
+	class ORG_Music_SW
+	{
+		author="Magic";
+		displayName="[ORG] Star Wars";
+	};
+	class ORG_Music_CP2077
+	{
+		author="Magic";
+		displayName="[ORG] Cyberpunk 2077";
+	};
+	class ORG_Music_Other
+	{
+		author="Magic";
+		displayName="[ORG] Other";
+	};
+};
---
+++ 332nd Legion Misc Mod/mod.cpp
@@ -0,0 +1,6 @@
+name = "332nd Legion Misc Mod";
+picture = "Logo.paa";
+overview = "332nd Legion Misc Mod";
+action = "https://www.youtube.com/watch?v=VVggXZPCuYw";
+logo = "Logo.paa";
+logoOver = "Logo.paa";
